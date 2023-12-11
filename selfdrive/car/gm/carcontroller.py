bdae9eaa730e500acf12d25bf23deba3c577276f
geniuth <74520084+geniuth@users.noreply.github.com>
2023-12-11 14:13:33 +0900

    Update carcontroller.py

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -112,19 +112,20 @@ class CarController():
       # self.pedal_hyst_gap = interp(CS.out.vEgo, [40.0 * CV.KPH_TO_MS, 100.0 * CV.KPH_TO_MS], [0.01, 0.006])
       # self.pedal_final, self.pedal_steady = actuator_hystereses(self.comma_pedal_original, self.pedal_steady, self.pedal_hyst_gap)
       # self.comma_pedal = clip(self.pedal_final, 0., 1.)
-      self.pedal_gas_max = interp(CS.out.vEgo, [0.0, 5, 30], [0.2725, 0.3275, 0.3650])
+      #self.pedal_gas_max = interp(CS.out.vEgo, [0.0, 5, 30], [0.2725, 0.3275, 0.3650])
 
-      accGain = 0.1429
-      accGain3 = interp(actuators.accel, [-3.5, 2], [0.185, 0.130])
-      accGain10 = interp(actuators.accel, [-3.5, 2], [0.245, 0.185])
-      accGain15 = interp(actuators.accel, [-3.5, 2], [0.25, 0.215])
-      zero = interp(CS.out.vEgo, [0., 3, 10, 15, 30], [0, accGain3, accGain10, accGain15, 0.280])
+      #accGain = 0.1429
+      #accGain3 = interp(actuators.accel, [-3.5, 2], [0.185, 0.130])
+      #accGain10 = interp(actuators.accel, [-3.5, 2], [0.245, 0.185])
+      #accGain15 = interp(actuators.accel, [-3.5, 2], [0.25, 0.215])
+      #zero = interp(CS.out.vEgo, [0., 3, 10, 15, 30], [0, accGain3, accGain10, accGain15, 0.280])
 
-      self.comma_pedal = clip((actuators.accel * accGain + zero), 0., 1.)
+      #self.comma_pedal = clip((actuators.accel * accGain + zero), 0., 1.)
 
-      #self.pedal_hyst_gap = interp(CS.out.vEgo, [40.0 * CV.KPH_TO_MS, 100.0 * CV.KPH_TO_MS], [0.01, 0.0050])
-      #pedal_final, self.pedal_steady = actuator_hystereses(self.comma_pedal, self.pedal_steady, self.pedal_hyst_gap)
-      #self.comma_pedal = clip(pedal_final, 0., self.pedal_gas_max)
+      zero = 0.15625  # 40/256
+      accGain = interp(abs(actuators.accel), [0, 1.5],[0.2, 1-zero])
+
+      self.comma_pedal = clip(zero + accGain * actuators.accel, 0., 1)      
 
       actuators.commaPedalOrigin = self.comma_pedal
 
